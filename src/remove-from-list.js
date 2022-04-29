409e87f1341955b289da01280056de52f212826a
Sergey M <marchuk.se@gmail.com>
2022-04-29 07:23:58 +0700

    stack

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -11,20 +11,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * @return {List}
  *
  * @example
- * For l = [3, 1, 2, 3, 4, 5] and k = 3,
- * the output should be [1, 2, 4, 5]
- *
- * Singly - linked lists are already defined using interface
- * class ListNode {
- *   constructor(x) {
- *     this.value = x;
- *     this.next = null;
- *   }
- * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+// For l = [3, 1, 2, 3, 4, 5] and k = 3,
+// the output should be[1, 2, 4, 5]
+
+// Singly - linked lists are already defined using interface
+class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
+  }
+}
+function removeKFromList(l, k) {
+  let tmp = l;
+  while (tmp !== null) {
+    if (tmp.value === k) {
+
+    }
+  }
 }
 
 module.exports = {
--- src/stack.js
+++ src/stack.js
@@ -4,30 +4,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement the Stack with a given interface via array.
  *
  * @example
- * const stack = new Stack();
- *
- * stack.push(1); // adds the element to the stack
- * stack.peek(); // returns the peek, but doesn't delete it, returns 1
- * stack.pop(); // returns the top element from stack and deletes it, returns 1
- * stack.pop(); // undefined
- *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.arr = [];
+  }
+
+  push(element) {
+    return this.arr.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr[this.arr.length - 1];
   }
 }
+
+const stack = new Stack();
+console.log(stack.push(1));
+console.log(stack.peek());
+console.log(stack.pop());
+console.log(stack.pop());
 
 module.exports = {
   Stack
